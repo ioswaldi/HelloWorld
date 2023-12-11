b9975a8d8ddc7b302c6294f504e2d10077490be1
waldi <febrianda.waldi@gmail.com>
2023-12-11 22:31:31 +0700

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Waldi Febrianda on 11/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello MOTHEFUCKER!!!!!!!")
+        print("Hello MOTHEFUCKER!!!!!!!")
+        print("Hello MOTHEFUCKER!!!!!!!")
+        print("Hello MOTHEFUCKER!!!!!!!")
+        print("Hello MOTHEFUCKER!!!!!!!")
+        
+    }
+    
+    
+}
+
